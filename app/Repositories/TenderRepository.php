0fd4187f7ca2ecd691427cde6e9bcbf74a983243
amazbhola <amazingbhola@gmail.com>
2023-01-03 19:57:28 +0600

    Php artisan Other Command Packege install like Repository and Interfaces, view

---
+++ app/Repositories/TenderRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\TenderRepositoryInterface;
+
+class TenderRepository implements TenderRepositoryInterface
+{
+    public function __construct()
+    {
+        //
+    }
+}
